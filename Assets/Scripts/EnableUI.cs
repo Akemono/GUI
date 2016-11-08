3cb82cd3a8bfea092794748fa96ad6425c7a5103
Akemono <mikematrix@gmail.com>
2016-11-08 10:24:35 +0100

    Commit before remove from old laptop

---
+++ Assets/Scripts/EnableUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableUI : MonoBehaviour {
+
+	public GameObject UI;
+
+	void Update() 
+	{
+		if (Input.GetButtonDown ("Cancel"))
+			UI.SetActive (true);
+	}
+}
---
+++ Assets/Scripts/Quitgame.cs
@@ -0,0 +1,9 @@
+﻿ using UnityEngine;
+ using System.Collections;
+
+ public class Quitgame : MonoBehaviour {
+
+	public void OnClick()	{
+		Application.Quit ();
+	}
+}
